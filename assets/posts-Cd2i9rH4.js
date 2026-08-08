7beac6ca97f0cd85dcf78cded427cbdc97cbf074
Aryan <work.aryanshah16@gmail.com>
2026-08-08 18:41:38 -0400

    Updates

--- assets/posts-Cd2i9rH4.js
+++
@@ -1,1 +0,0 @@
-var e=[{slug:`data-pipeline`,date:`2026-08-09`,dateLabel:`August 9, 2026`,title:`One take in a kitchen, four ways of seeing it`,excerpt:`I strapped a camera to my head, diced vegetables for twenty minutes, and ran the tape through our pipeline. Here's what actually comes out the other end.`,readingTime:`6 min read`}];function t(t){return e.find(e=>e.slug===t)}export{e as n,t};
